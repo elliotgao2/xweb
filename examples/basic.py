eb3ba4de0f33810c735ea78c6dd2164bf02e1de3
elliot <elliotgao@xwfintech.com>
2018-09-27 16:47:21 +0800

    dev2.0

--- examples/basic.py
+++ examples/basic.py
@@ -1,69 +1,17 @@
-from xweb.application import XWeb
-from xweb.exception import abort
-from xweb.globals import request, response
+from xweb import XWeb
 
 app = XWeb()
 
 
-@app.middleware('request')
-def print_on_request1():
-    print("I print when a request is received by the server1")
+def index(ctx):
+    return "1111"
 
 
-@app.middleware('response')
-def print_on_response1():
-    print("I print when a response is returned by the server1")
+def home(ctx):
+    return "2222"
 
 
-@app.middleware('response')
-def print_on_response2():
-    print("I print when a response is returned by the server2")
+app.add_route('/', index)
+app.add_route('/home', home)
 
-
-@app.route('/', methods=['GET', 'POST'])
-def hello():
-    return '<h1>hello world</h1>'
-
-
-@app.route('/environ')
-def environ():
-    return str(request.environ)
-
-
-@app.route('/headers/')
-def headers():
-    return request.headers.store
-
-
-@app.route('/forms/', methods=['POST'])
-def forms():
-    return request.forms
-
-
-@app.route('/files/', methods=['POST'])
-def files():
-    return request.files['a'].filename
-
-
-@app.route('/query/')
-def query():
-    return request.query
-
-
-@app.post('/post')
-def post():
-    return request.forms
-
-
-@app.get('/exception')
-def post():
-    abort(500)
-    return "OK"
-
-
-@app.exception(500)
-def exception():
-    response.body = "FAIL"
-
-
-app.listen(3000)
+app.run()
