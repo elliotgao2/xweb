bad8cd3e3a60bc27316b4ac7bdc7ed18b6dce905
elliot <elliotgao@xwfintech.com>
2018-09-29 16:02:10 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('xweb.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+            f.read().decode('utf-8')).group(1)))
 
 setup(name='xweb',
       version=version,
       description='High performance web framework built with uvloop and httptools.',
       author='gaojiuli',
+      long_description=open('README.md', 'r', encoding='utf8').read(),
+      long_description_content_type='text/markdown',
       author_email='gaojiuli@gmail.com',
       url='https://github.com/gaojiuli/xweb',
       py_modules=['xweb'],
